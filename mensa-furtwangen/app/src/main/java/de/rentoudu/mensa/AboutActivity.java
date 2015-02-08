f880b27f0b1fdde4d4ed6167bf54f84813f0f4e4
crysxd <c.wuerthner@me.com>
2015-02-08 17:44:53 +0100

    Updated AboutActivity

--- mensa-furtwangen/app/src/main/java/de/rentoudu/mensa/AboutActivity.java
+++ mensa-furtwangen/app/src/main/java/de/rentoudu/mensa/AboutActivity.java
@@ -3,6 +3,7 @@ package de.rentoudu.mensa;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
 import de.hfu.mensa.R;
@@ -25,8 +26,11 @@ public class AboutActivity extends DefaultActivity {
 		
 		String message = String.format(getString(R.string.text_about), version);
 		((TextView) this.findViewById(R.id.text1)).setText(message);
-		
-		this.getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Set up Toolbar
+        Toolbar bar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(bar);
+		this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 	}
 	
