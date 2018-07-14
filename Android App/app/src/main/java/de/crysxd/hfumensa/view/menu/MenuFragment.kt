91fae227c5227a323fd70b512e2b1e81ee554d79
Christian Würthner <c.wurthner@gmail.com>
2018-07-14 21:35:24 +0200

    Add menu fragment

---
+++ Android App/app/src/main/java/de/crysxd/hfumensa/view/menu/MenuFragment.kt
@@ -0,0 +1,13 @@
+package de.crysxd.hfumensa.view.menu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.crysxd.hfumensa.R
+
+class MenuFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_menu, container, false)
+
+}
