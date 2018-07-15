60cdd3938fd22f012443c454a8df56143e7680a0
Christian Würthner <c.wurthner@gmail.com>
2018-07-15 10:59:11 +0200

    Style selection screen

--- Android App/app/src/main/java/de/crysxd/hfumensa/view/selectcanteen/SelectCanteenFragment.kt
+++ Android App/app/src/main/java/de/crysxd/hfumensa/view/selectcanteen/SelectCanteenFragment.kt
@@ -2,24 +2,35 @@ package de.crysxd.hfumensa.view.selectcanteen
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import de.crysxd.hfumensa.R
 import de.crysxd.hfumensa.persistence.CanteenRepository
-import de.crysxd.hfumensa.persistence.MenuRepository
 import de.crysxd.hfumensa.view.ErrorDialogHelper
 import kotlinx.android.synthetic.main.fragment_select_canteen.*
-import timber.log.Timber
-import java.util.*
 
-class SelectCanteenFragment : Fragment() {
+class SelectCanteenFragment : Fragment(), OnMapReadyCallback {
 
     var shownAlertDialog: AlertDialog? = null
     val adapter = CanteenAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) = inflater.inflate(R.layout.fragment_select_canteen, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
 
     override fun onStart() {
         super.onStart()
@@ -34,5 +45,11 @@ class SelectCanteenFragment : Fragment() {
             shownAlertDialog?.dismiss()
             shownAlertDialog = ErrorDialogHelper.showErrorDialog(context, it, R.string.ui_error_unable_to_load_data)
         })
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
