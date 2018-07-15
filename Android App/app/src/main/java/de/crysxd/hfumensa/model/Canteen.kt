13443d5dce481009b4c50505ea0199e05711a78f
Christian Würthner <c.wurthner@gmail.com>
2018-07-15 15:33:24 +0200

    Update layout

--- Android App/app/src/main/java/de/crysxd/hfumensa/model/Canteen.kt
+++ Android App/app/src/main/java/de/crysxd/hfumensa/model/Canteen.kt
@@ -1,15 +1,13 @@
 package de.crysxd.hfumensa.model
 
-class Canteen() {
-    var id: Int = 0
-    lateinit var name: String
-    lateinit var street: String
-    lateinit var zip: String
-    lateinit var place: String
-    lateinit var menuUrl: String
-    lateinit var description: String
-    lateinit var openingHours: String
-    lateinit var iconUrl: String
-    var latitude: Float = 0.0f
-    var longitude: Float = 0.0f
-}
+data class Canteen(var id: Int = 0,
+    var name: String = "",
+    var street: String = "",
+    var zip: String = "",
+    var place: String = "",
+    var menuUrl: String = "",
+    var description: String = "",
+    var openingHours: String = "",
+    var iconUrl: String = "",
+    var latitude: Float = 0.0f,
+    var longitude: Float = 0.0f)
--- Android App/app/src/main/java/de/crysxd/hfumensa/view/selectcanteen/SelectCanteenFragment.kt
+++ Android App/app/src/main/java/de/crysxd/hfumensa/view/selectcanteen/SelectCanteenFragment.kt
@@ -1,12 +1,14 @@
 package de.crysxd.hfumensa.view.selectcanteen
 
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.PagerSnapHelper
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,6 +19,9 @@ import de.crysxd.hfumensa.R
 import de.crysxd.hfumensa.persistence.CanteenRepository
 import de.crysxd.hfumensa.view.ErrorDialogHelper
 import kotlinx.android.synthetic.main.fragment_select_canteen.*
+import androidx.recyclerview.widget.RecyclerView
+
+
 
 class SelectCanteenFragment : Fragment(), OnMapReadyCallback {
 
@@ -36,15 +41,45 @@ class SelectCanteenFragment : Fragment(), OnMapReadyCallback {
         super.onStart()
 
         recyclerView.adapter = adapter
+        val snapHelper = object: PagerSnapHelper() {
+            override fun findTargetSnapPosition(layoutManager: RecyclerView.LayoutManager, velocityX: Int, velocityY: Int): Int {
+                val snapPosition = super.findTargetSnapPosition(layoutManager, velocityX, velocityY)
+                onCanteenSelected(snapPosition)
+                return snapPosition
+            }
+        }
+        snapHelper.attachToRecyclerView(recyclerView)
 
         val (result, error) = CanteenRepository().getCanteens()
         result.observe(this, Observer {
-            adapter.canteens = it
+            adapter.canteens = it.sortedBy {
+                it.name
+            }
+            onCanteenSelected(0)
         })
         error.observe(this, Observer {
             shownAlertDialog?.dismiss()
             shownAlertDialog = ErrorDialogHelper.showErrorDialog(context, it, R.string.ui_error_unable_to_load_data)
         })
+    }
+
+    fun onCanteenSelected(position: Int) {
+        TransitionManager.beginDelayedTransition(view as ViewGroup)
+        buttonNext.visibility = if (adapter.itemCount == 0 || position >= adapter.itemCount - 1) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+        buttonPrevious.visibility = if (adapter.itemCount == 0 || position <= 0) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+        buttonContinue.visibility =  if (adapter.itemCount == 0) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 
     override fun onMapReady(map: GoogleMap) {
