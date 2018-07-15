f885cb3f92376982a93728593e851f38370282ea
Christian Würthner <c.wurthner@gmail.com>
2018-07-15 15:48:57 +0200

    Add action to next and previous buttons

--- Android App/app/src/main/java/de/crysxd/hfumensa/view/selectcanteen/SelectCanteenFragment.kt
+++ Android App/app/src/main/java/de/crysxd/hfumensa/view/selectcanteen/SelectCanteenFragment.kt
@@ -42,13 +42,45 @@ class SelectCanteenFragment : Fragment(), OnMapReadyCallback {
 
         recyclerView.adapter = adapter
         val snapHelper = object: PagerSnapHelper() {
+            var snappedPosition = 0
+            private var snapToNext = false
+            private var snapToPrevious = false
+
             override fun findTargetSnapPosition(layoutManager: RecyclerView.LayoutManager, velocityX: Int, velocityY: Int): Int {
-                val snapPosition = super.findTargetSnapPosition(layoutManager, velocityX, velocityY)
-                onCanteenSelected(snapPosition)
-                return snapPosition
+                if (snapToNext) {
+                    snapToNext = false
+                    snappedPosition = Math.min(recyclerView.adapter?.itemCount ?: 0, snappedPosition + 1)
+                } else if (snapToPrevious) {
+                    snapToPrevious = false
+                    snappedPosition = Math.max(0, snappedPosition - 1)
+                } else {
+                    snappedPosition = super.findTargetSnapPosition(layoutManager, velocityX, velocityY)
+                }
+
+                onCanteenSelected(snappedPosition)
+                return snappedPosition
+            }
+
+            fun snapToNext() {
+                snapToNext = true
+                onFling(Int.MAX_VALUE, Int.MAX_VALUE)
+            }
+
+            fun snapToPrevious() {
+                snapToPrevious = true
+                onFling(Int.MAX_VALUE, Int.MAX_VALUE)
             }
         }
         snapHelper.attachToRecyclerView(recyclerView)
+
+        buttonNext.setOnClickListener {
+            snapHelper.snapToNext()
+        }
+
+        buttonPrevious.setOnClickListener {
+            snapHelper.snapToPrevious()
+        }
+
 
         val (result, error) = CanteenRepository().getCanteens()
         result.observe(this, Observer {
